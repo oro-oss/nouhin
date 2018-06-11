ed1a031af3022dd10d5976b1d86c5f6970640316
ktsn <ktsn55@gmail.com>
2018-06-11 15:43:31 +0900

    reset fixtures after each test

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,35 +5,50 @@ const { execSync } = require('child_process')
 const fixtureBase = path.resolve(__dirname, 'fixtures')
 
 function test(fixtureType) {
+  const cwd = path.join(fixtureBase, fixtureType)
+  const hash = execSync('git rev-parse HEAD', { cwd })
+
   function nouhin(args = '') {
     execSync(path.resolve(__dirname, '../bin/nouhin.js ') + args, {
-      cwd: path.join(fixtureBase, fixtureType)
+      cwd
     })
   }
 
   function read(file) {
-    return fs.readFileSync(path.join(fixtureBase, fixtureType, file))
+    return fs.readFileSync(path.join(cwd, file))
   }
 
   function exists(file) {
-    return fs.existsSync(path.join(fixtureBase, fixtureType, file))
+    return fs.existsSync(path.join(cwd, file))
   }
 
   function equals(expected, actual) {
     expect(read(expected)).toEqual(read(actual))
   }
 
+  function reset() {
+    execSync('git reset --hard ' + hash, { cwd })
+    execSync('git clean -df', { cwd })
+  }
+
   return {
     nouhin,
     read,
     exists,
-    equals
+    equals,
+    reset
   }
 }
 
 describe('Nouhin', () => {
+  let t
+
+  afterEach(() => {
+    t.reset()
+  })
+
   it('adds delivery directory', () => {
-    const t = test('init')
+    t = test('init')
     t.nouhin()
     t.exists('.nouhin')
     t.equals('foo.txt', '.nouhin/foo.txt')
